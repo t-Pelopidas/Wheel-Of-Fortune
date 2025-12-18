e5e45468b4c848085d44125cb75107b61a1dd170
t.Pelopidas <thomaspelopidas12@gmail.com>
2025-12-18 20:28:41 +0200

    VERY basic completed | Cant figure out the QQ exit method

--- client.c
+++ client.c
@@ -1,24 +1,72 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
-#include <stdbool.h>
+
+#define MAX_WORD_LENGTH 1024
 
 void die(const char* msg){
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
+struct sockaddr_in init_client(int *server_fd,char* ip, char* port){
+    
+    struct sockaddr_in server_addr;
 
-int main(int argc, char** argv){
+    if((*server_fd = socket(AF_INET,SOCK_STREAM, 0)) < 0 ) die("Socket Failed");
+    printf("Socket Completed\n");
 
-    if(argc < 2) {
+    server_addr.sin_family = AF_INET;
+
+    int int_port = atoi(port);
+    server_addr.sin_port = htons(int_port);
+
+    if(inet_aton(ip, &server_addr.sin_addr) == 0) die("Conversion from ARGUMENT to ADDRESS failed");
+    printf("inet_aton Completed\n");
+    
+    return server_addr;
+} 
+
+int main(int argc,char** argv){
+
+    char buffer[MAX_WORD_LENGTH];
+    if(argc < 3) {
         printf("Usage: ./<program> <IP Address> <Port>\n");
         die("Wrong Usage");
     }
+    int server_fd;
 
-    printf("IP: %s, Port: %d\n",argv[1], atoi(argv[2]));
+    struct sockaddr_in server_addr = init_client(&server_fd, argv[1], argv[2]);
+    socklen_t server_addr_len = sizeof(server_addr);
+
+
+    if (connect(server_fd,(struct sockaddr *)&server_addr,server_addr_len) < 0) die("Connect Failed");
+    printf("Connection Established\n");
+
+    printf("%s, %d, %d\n", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port), server_fd);
+
+    int r_bytes;
+
+    if((r_bytes = recv(server_fd,buffer, MAX_WORD_LENGTH, 0)) < 0) die("Failed to receive message");
+    buffer[r_bytes] = '\0';
+
+    while(1){
+
+        printf("From server: %s", buffer);
+
+        fflush(stdout);
+        printf("To Server: ");
+        fflush(stdin);
+        fgets(buffer, MAX_WORD_LENGTH, stdin);
+
+        if(send(server_fd, buffer, strlen(buffer), 0) < 0 ) die("Failed to send message");
+
+        if((r_bytes = recv(server_fd,buffer, MAX_WORD_LENGTH, 0)) < 0) die("Failed to receive message");
+        buffer[r_bytes] = '\0';
+    }
+
 }
--- server.c
+++ server.c
@@ -9,8 +9,10 @@
 
 #define PORT 4001
 #define MAX_PLAYERS 1
-#define MAX_WORD_LENGHT 256
+#define MAX_WORD_LENGHT 1024
 #define OPENING_QUOTE "HELLO EVERONE\nWELCOME TO THE WHEEL OF FORTUNE\nITS TIME TO START THE GAME\n"
+
+char buffer[MAX_WORD_LENGHT];
 
 void die(const char* msg){
     perror(msg);
@@ -33,11 +35,7 @@ GameState init_game(){
 }
 
 void send_to(int fd, const char* msg){
-
-
-    write(fd, msg, strlen(msg));
-
-    return;
+    if(send(fd, msg, strlen(msg), 0) < 0) die("Failed to send message");
 }
 
 void broadcast(GameState G,const char* msg){
@@ -47,12 +45,12 @@ void broadcast(GameState G,const char* msg){
     }
     return;
 }
-void accept_clients(GameState *G,int server_socket, struct sockaddr_in server_address){
+void accept_clients(GameState *G,int server_fd, struct sockaddr_in server_address){
 
     socklen_t server_address_size = sizeof(server_address);
 
     for(int i = 0; i<MAX_PLAYERS; i++){
-        if ((G->client_fds[i] = accept(server_socket, (struct sockaddr *)&server_address, &server_address_size)) < 0 ) die("accept failed"); 
+        if ((G->client_fds[i] = accept(server_fd, (struct sockaddr *)&server_address, &server_address_size)) < 0 ) die("accept failed"); 
         printf("P%d accepted with fd:%d \n", i, G->client_fds[i]);
     }
 
@@ -80,24 +78,23 @@ void close_clients(GameState G){
 
     return;
 }
-struct sockaddr_in init_server(int *server_socket){
+struct sockaddr_in init_server(int *server_fd){
     struct sockaddr_in server_addr;
 
-    if((*server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) die("Server socket failed");
+    if((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) die("Server socket failed");
     printf("Socket Completed\n");
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
     socklen_t server_addr_size = sizeof(server_addr);
 
-    printf("%d, %d, %d, %d\n", server_addr.sin_family, server_addr.sin_addr.s_addr, server_addr.sin_port, *server_socket);
+    printf("%d, %d, %d, %d\n", server_addr.sin_family, server_addr.sin_addr.s_addr, server_addr.sin_port, *server_fd);
 
-    if(bind(*server_socket,(struct sockaddr *)&server_addr, server_addr_size) < 0) die("Bind failed");
+    if(bind(*server_fd,(struct sockaddr *)&server_addr, server_addr_size) < 0) die("Bind failed");
     printf("Bind Completed\n");
 
-    if(listen(*server_socket,MAX_PLAYERS) < 0) die("Listen failed");
+    if(listen(*server_fd,MAX_PLAYERS) < 0) die("Listen failed");
     printf("Listen Completed\n");
-
 
     return server_addr;
 
@@ -106,26 +103,33 @@ int main(){
 
     GameState Game = init_game();
 
-    int server_socket;
+    int server_fd, r_bytes;
 
-    struct sockaddr_in server_addr = init_server(&server_socket);
+    struct sockaddr_in server_addr = init_server(&server_fd);
 
-    print_game_state(Game);
-
-    printf("Socket Completed\n");
-
-    accept_clients(&Game,server_socket, server_addr);
+    accept_clients(&Game,server_fd, server_addr);
 
     broadcast(Game,OPENING_QUOTE);
 
-    for(int i = 0; i<MAX_PLAYERS; i++){
-        send_to(Game.client_fds[i],"HELLO\n");
-    }
-
     print_game_state(Game);
 
+    if ((r_bytes = recv(Game.client_fds[0], buffer,MAX_WORD_LENGHT, 0)) < 0) die("Failed to receive message");
+    buffer[r_bytes] = '\0';
+
+    while(strcmp(buffer, "QQ") != 0){
+
+        printf("From P%d: %s",0, buffer);
+
+        fflush(stdin);
+        fgets(buffer, MAX_WORD_LENGHT, stdin);
+
+        send_to(Game.client_fds[0],buffer);
+
+        if ((r_bytes = recv(Game.client_fds[0], buffer,MAX_WORD_LENGHT, 0)) < 0) die("Failed to receive message");
+        buffer[r_bytes] = '\0';
+    }
     close_clients(Game);
 
-    close(server_socket);
+    close(server_fd);
 
 }
